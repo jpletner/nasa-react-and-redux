5fab29c6cc5dbea79b8e545676d2d017d6f114e3
Jenny Pletner <jpletner@gapintelligence.com>
2018-01-17 12:14:50 -0800

    Implements reducers and actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'; // import the main react dependency
 import ReactDOM from 'react-dom'; // import reactDOM
-import { Provider } from 'react-redux'; // glue for react and redux
+import { Provider } from 'react-redux'; // Glue for react and redux
+import Store from './Store'; // redux state store
 import App from './app/App'; // import the main app component
 
 import './assets/stylesheets/styles.scss';
 
 ReactDOM.render(
-  <Provider> {/* Glue for react and redux */}
-    <App /> {/* Render or App component */}
-  </Provider>,
-  document.getElementById('root') // mount app to our #root element
-); 
+	<Provider store={Store}>
+		<App />
+	</Provider>,
+	document.getElementById('root') // and mount it to our #root element
+);
